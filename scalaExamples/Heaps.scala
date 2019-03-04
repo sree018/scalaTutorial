c6b844ba31d2bae794904a56ee2005db4531b48d
sree018 <sdama018@gmail.com>
2019-03-04 13:02:52 -0500

    Create Heaps.scala

---
+++ scalaExamples/Heaps.scala
@@ -0,0 +1,19 @@
+package com.scalaDataStructures
+
+object Heaps {
+  def main(args:Array[String]){
+    
+  }
+  def StandardBinaryHeap(){
+    
+  }
+	def LeftistHeap(){
+    
+  }
+	def PairingHeap(){
+    
+  }
+	def SkewHeap(){
+    
+  }
+}
